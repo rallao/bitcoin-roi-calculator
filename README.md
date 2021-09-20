29b71e0e1f9ab412cc6c3c8609fd38d0fb818d10
rallao85@gmail.com <rallao85@gmail.com>
2021-09-20 15:25:48 -0300

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ga-project-1
+# Bitcoin Calculator
+
+# Techonologies Used
+- HTML
+- CSS
+- Javascript/jQuery
+
+# Screenshots
+
+# Getting Started
+
+# Future Enchacements
--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Im working")
+// const and variables
+const bitcoinPrice = $.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+
+console.log(bitcoinPrice);
+
+//$.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').then(function(data) {
+//    console.log(data)
+//}) ;
