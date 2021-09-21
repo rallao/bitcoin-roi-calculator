309283b4c2c2d4f173d259a78b37f80f8086755a
rallao85@gmail.com <rallao85@gmail.com>
2021-09-21 09:40:47 -0300

    WIP: fetch BTC price

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 // const and variables
+
+const BASE_URL = 'https://api.coingecko.com/api/v3/simple/price'
 const bitcoinPrice = $.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
 
-console.log(bitcoinPrice);
+let currentPrice = ''
+let historicalPrice = ''
 
-//$.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').then(function(data) {
-//    console.log(data)
-//}) ;
+
+
+// Functions
+
+// Bitcoin Current price
+$.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').then(function(data) {
+    currentPrice = data;
+    console.log(currentPrice.bitcoin.usd);
+}, function(error){
+    console.log(error);
+});
+
+
+// Bitcoin historical price
+$.ajax('https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2017').then(function(data) {
+    historicalPrice = data
+    console.log(historicalPrice.market_data.current_price.usd);
+}, function(error){
+    console.log(error);
+});
