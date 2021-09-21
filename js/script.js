f8829752ce1d312d918e7267a5d1c942072ce3bb
rallao85@gmail.com <rallao85@gmail.com>
2021-09-21 17:04:10 -0300

    WIP: update api calls so now have variables

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 // const and variables
-
 const BASE_URL = 'https://api.coingecko.com/api/v3/simple/price'
+const HIST_PRICE_API = 'https://api.coingecko.com/api/v3/coins/'
 const bitcoinPrice = $.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
 
 let currentPrice = ''
 let historicalPrice = ''
 
 
+// Setting temporary variables from the form
+let assetSelected = 'bitcoin';
+let amountSelected = 1000;
+let dateSelected = '30-12-2017'
 
 // Functions
 
 // Bitcoin Current price
-$.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').then(function(data) {
+$.ajax(`${BASE_URL}?ids=${assetSelected}&vs_currencies=usd`).then(function(data) {
     currentPrice = data;
     console.log(currentPrice.bitcoin.usd);
 }, function(error){
@@ -20,9 +24,10 @@ $.ajax('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=
 
 
 // Bitcoin historical price
-$.ajax('https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2017').then(function(data) {
+$.ajax(`${HIST_PRICE_API}${assetSelected}/history?date=${dateSelected}`).then(function(data) {
     historicalPrice = data
     console.log(historicalPrice.market_data.current_price.usd);
 }, function(error){
     console.log(error);
 });
+
