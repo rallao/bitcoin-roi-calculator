c763a79da4963e570c32086c5299e2431f1f5164
rallao85@gmail.com <rallao85@gmail.com>
2021-09-23 14:17:07 -0300

    added footer, style and callback functions

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 const CURRENT_PRICE_API_URL = 'https://api.coingecko.com/api/v3/simple/price'
 const HIST_PRICE_API_URL = 'https://api.coingecko.com/api/v3/coins/'
 
-let currentPrice = ''
-let historicalPrice = ''
+let currentPrice;
+let historicalPrice;
 
 const $form = $('form');
 const $input = $('input');
@@ -17,6 +17,7 @@ let dateSelected; //date format needs to be in 'dd-mm-yyyy' for the api in order
 // Event listener when user click button
 function handleGetData (event){
     event.preventDefault();
+    // grab value from form and update variables.
     amountSelected = $("#amount-data").val();
     assetSelected = $("#asset-data").val();
     dateSelected = $("#date-data").val();
@@ -28,34 +29,36 @@ function handleGetData (event){
     console.log(dateSelected);
     console.log(assetSelected);
 
-    // call the functions that fetch the data.
+    // call the functions that fetch api data.
     getHistoricalPrice();
     getCurrentPrice();
+
+    // call the function that makes the roi.
     returnOverInvestment();
 }
-// Bitcoin Current price
+// asset current price function.
 function getCurrentPrice() {$.ajax(`${CURRENT_PRICE_API_URL}?ids=${assetSelected}&vs_currencies=usd`).then(function(data) {
-    currentPrice = data.bitcoin.usd;
-    console.log(currentPrice);
+    currentPrice = data.assetSelected.usd;
+    console.log(`The current price of ${assetSelected} is ${currentPrice}`);
 }, function(error){
     console.log(error);
 });
 }
 
-// Bitcoin historical price
+// asset historical price funtcion.
 function getHistoricalPrice() {$.ajax(`${HIST_PRICE_API_URL}${assetSelected}/history?date=${dateSelected}`).then(function(data) {
     historicalPrice = data.market_data.current_price.usd
-    console.log(historicalPrice);
+    console.log(`The price of ${assetSelected} in ${dateSelected} was ${historicalPrice}`);
 }, function(error){
     console.log(error);
 });
 }
 
-// return over investment function.
-// function returnOverInvestment(){
-//     var roi = (currentPrice / historicalPrice) / historicalPrice * 100
-// };
-// console.log(roi);
+// Return over investment function.
+function returnOverInvestment(){
+    let roi = parseInt(100 * (currentPrice - historicalPrice) / historicalPrice);
+    console.log(roi);
+};
 
 
 // Call the handleGetData() function when user click
